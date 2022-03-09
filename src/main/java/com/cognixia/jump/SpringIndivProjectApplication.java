ec6027cbfa75cac088987dfb8a0e349fde1629f3
nathankaufman <nlk7665@rit.edu>
2022-03-09 10:32:16 -0500

    API definition documentation

--- src/main/java/com/cognixia/jump/SpringIndivProjectApplication.java
+++ src/main/java/com/cognixia/jump/SpringIndivProjectApplication.java
@@ -3,7 +3,11 @@ package com.cognixia.jump;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title="Monitor retail api", version = "0.9", description="API for the cognixia jump program for managing a online monitor store"))
 public class SpringIndivProjectApplication {
 
 	public static void main(String[] args) {
