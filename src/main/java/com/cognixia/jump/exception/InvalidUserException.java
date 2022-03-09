7658e115958b3a8a5a15e60b57e15d36466bfd5a
nathankaufman <nlk7665@rit.edu>
2022-03-08 23:38:40 -0500

    Exception constructor

--- src/main/java/com/cognixia/jump/exception/InvalidUserException.java
+++ src/main/java/com/cognixia/jump/exception/InvalidUserException.java
@@ -10,5 +10,9 @@ public class InvalidUserException extends Exception{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public InvalidUserException(String msg) {
+		super(msg);
+	}
 
 }
