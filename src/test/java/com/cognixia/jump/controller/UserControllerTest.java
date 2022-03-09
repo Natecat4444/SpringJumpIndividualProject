39a447b3b8d746a79e701ed0803c573ca73493b2
nathankaufman <nlk7665@rit.edu>
2022-03-09 11:30:57 -0500

    User tests, need to mock security somehow

--- src/test/java/com/cognixia/jump/controller/UserControllerTest.java
+++ src/test/java/com/cognixia/jump/controller/UserControllerTest.java
@@ -1,10 +1,38 @@
 package com.cognixia.jump.controller;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+import java.util.List;
+import java.util.Arrays;
+
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+import com.cognixia.jump.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
 	private final String STARTING_URI = "http://localhost:8080/api";
 	
@@ -15,12 +43,40 @@ public class UserControllerTest {
 	private UserController controller;
 	
 	@Test
-	void testGetUsers() {
-		//TODO
+	void testGetUsers() throws Exception {
+		String uri = STARTING_URI +"/user";
+		List<User> allUsers = Arrays.asList(new User(), new User());
+		
+		when( controller.getUsers()).thenReturn(allUsers);
+		
+		mvc.perform(get(uri)).andDo(print()).andExpect(status().isOk());
+		
+		verify(controller, times(1)).getUsers();
+		verifyNoMoreInteractions(controller);
 	}
 	
 	@Test
-	void testCreateUser() {
-		//TODO
+	void testCreateUser() throws Exception {
+		String uri = STARTING_URI +"/user";
+		
+		User user = new User(); 
+		
+		mvc.perform(post(uri)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content( asJsonString(user)))
+				.andDo( print() ).andExpect(status().isCreated());
+		
+		
+	}
+
+	public static String asJsonString( final Object obj ) {
+		
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+			
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+		
 	}
 }
