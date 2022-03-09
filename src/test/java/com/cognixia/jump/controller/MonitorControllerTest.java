84260309f4951f5a1488c71d40722db96b6f222a
nathankaufman <nlk7665@rit.edu>
2022-03-08 23:30:32 -0500

    Monitortest outline

--- src/test/java/com/cognixia/jump/controller/MonitorControllerTest.java
+++ src/test/java/com/cognixia/jump/controller/MonitorControllerTest.java
@@ -1,5 +1,61 @@
 package com.cognixia.jump.controller;
 
-public class MonitorControllerTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(MonitorController.class)
+public class MonitorControllerTest {
+	@Autowired
+	private MockMvc mvc;
+	
+	@InjectMocks
+	private MonitorController controller;
+	
+	private final String STARTING_URI = "http://localhost:8080/api";
+	
+	@Test
+	void testCreateMonitor() {
+		
+	}
+	
+	@Test
+	void testGetMonitors() {
+		
+	}
+	
+	@Test
+	void testGetMonitor() {
+		
+	}
+	
+	@Test
+	void testGetMonitorByBrand() {
+		
+	}
+	
+	@Test
+	void testGetMonitorBySize() {
+		
+	}
+	
+	@Test
+	void testGetMonitorBySizeGreaterThan() {
+		
+	}
+	
+	@Test
+	void testGetMonitorBySizeAndBrand() {
+		
+	}
+	
+	@Test
+	void testGetMonitorBySizeAndBrandGreaterThan() {
+		
+	}
 }
